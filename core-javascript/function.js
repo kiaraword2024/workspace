2a50caf205531b4ef2a108f705a0f64008ec905e
kiaraword2024 <chiarapolo@yahoo.it>
2024-09-30 11:40:21 +0200

    Update function.js

--- core-javascript/function.js
+++ core-javascript/function.js
@@ -1,13 +1,3 @@
-/*let scope ="global";
-
-function checkScope(){
-    let scope ="functionScope";
-    function inside(){return scope};
-    return inside;
-}
-console.log(checkScope()());
-console.log(scope);
-*/
 let nombre ="Kiara";
 
 function Nombrar(){
@@ -19,3 +9,37 @@ function Nombrar(){
     }
 }
 console.log(Nombrar());
+function saludar (Hola, Nombre){
+    return ("Hola"+"Nombre")
+}
+let saludarKiara = "Hola"+ "Kiara";
+console.log(saludarKiara);
+//ej 2 tema 5//
+function clickBoton(){
+    return ("Te saludo")
+}
+
+console.log(clickBoton());
+//ej4 tema 5//
+
+let email= "nombre1@gmail.com"
+
+function correo(){
+    if(email == "nombre@gmail.com")
+        return ("Email correcta")
+    else{
+        return("Email no correcta");
+    }
+  }
+console.log(correo());
+
+//función numero random de 1 a 10//
+function getRandomNumber(){
+    const random =Math.random()
+        const multiplied = random * 10
+        const rounded =Math.floor(multiplied)
+        const result = rounded +1
+        return result
+    }
+getRandomNumber()
+console.log(getRandomNumber());
