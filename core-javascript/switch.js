d61b955e4c731929c86d60c0b477f78840686099
kiaraword2024 <chiarapolo@yahoo.it>
2024-10-08 13:15:24 +0200

    Update switch.js

--- core-javascript/switch.js
+++ core-javascript/switch.js
@@ -1,4 +1,4 @@
-/*let comprar = "listar"
+let comprar = "listar"
 switch (comprar){
     case "listar":
         console.log("hacer la lista de la compra");
@@ -7,13 +7,4 @@ switch (comprar){
         console.log("tirar la lista de la compra");
         break;
 }
-*/
-let comprar = "listar"
-if(comprar =="listar"){
-    console.log("hacer la lista de la compra");
-}
-else if (comprar =="borrar"){
-       console.log("tirar la lista de la compra");
-}
-
 
