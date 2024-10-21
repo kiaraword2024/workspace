27180984a32d4c871dec1a3fd3001225eb378dd5
kiaraword2024 <chiarapolo@yahoo.it>
2024-10-21 14:21:33 +0200

    Update Rueda.js

--- core-javascript/Rueda.js
+++ core-javascript/Rueda.js
@@ -1,21 +1,4 @@
-/*let diametro =1.2
-let grosor =0.2
 
-function RuedaFiat(){
-    if (diametro >1.4)
-        return ("La rueda es para un vehiculo grande");
-    else if(diametro <=1.4 && diametro > 0.8)
-        return ("La rueda es por un vehiculo mediano");
-    else if(diametro >1.4 && grosor >0.8)
-        return("El grosor por esta rueda es el inferior al recomendado");
-    else if(diametro <=1.4 && diametro > 0.8 && grosor <0.25)
-        return("El grosor por esta rueda es el inferior al recomendado");
-    else
-    return ("La rueda es para un vehiculo pequeno");
-  }
-
-console.log(RuedaFiat());
-*/
 let diametro =0.7
 let grosor =0.1
 let FiatStrada = diametro >1.4
